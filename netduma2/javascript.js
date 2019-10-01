829bc94790e11c0be9f9b8c1869dba7b90f22a76
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-10-01 16:38:39 +0100

    Update javascript.js

--- netduma2/javascript.js
+++ netduma2/javascript.js
@@ -50,15 +50,6 @@ document.getElementById("create").addEventListener("click", function(){
     
     localStorage.setItem('users', JSON.stringify(users))
     local = JSON.parse(localStorage.getItem('users'))
-
-document.getElementsByTagName('h1')[0].innerHTML = local.name;
-document.getElementsByTagName('h1')[1].innerHTML = local.lastname;
-document.getElementsByTagName('h1')[2].innerHTML = local.telephone;
-document.getElementsByTagName('h1')[3].innerHTML = local.address;
-document.getElementsByTagName('h1')[4].innerHTML = local.street;
-document.getElementsByTagName('h1')[5].innerHTML = local.town;
-document.getElementsByTagName('h1')[6].innerHTML = local.country;
-document.getElementsByTagName('h1')[7].innerHTML = local.name;
   });
 
   document.getElementById("clear").addEventListener("click", function(){
@@ -77,14 +68,7 @@ document.getElementsByTagName('h1')[7].innerHTML = local.name;
       
   });
 
-  document.getElementById('name').textContent = JSON.parse(localStorage.getItem('users')).name
-  document.getElementById('lastname').textContent = JSON.parse(localStorage.getItem('users')).lastname
-  document.getElementById('telephone').textContent = JSON.parse(localStorage.getItem('users')).telephone
-  document.getElementById('address').textContent = JSON.parse(localStorage.getItem('users')).address
-  document.getElementById('street').textContent = JSON.parse(localStorage.getItem('users')).street
-  document.getElementById('email').textContent = JSON.parse(localStorage.getItem('users')).email
-  document.getElementById('town').textContent = JSON.parse(localStorage.getItem('users')).town
-  document.getElementById('country').textContent = JSON.parse(localStorage.getItem('users')).country
+ document.getElementById('output').textContent = (localStorage.getItem('users'))
 
 
 
